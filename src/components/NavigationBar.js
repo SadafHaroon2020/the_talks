bf0bc51608464a5de7b0108b61d906e5f1c44816
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-24 23:05:17 -0500

    making navigation

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as router,
+Switch,
+Link,
+Route
+} from "react-router-dom"
 
 class NavigationBar extends Component.React{
     render(){
         return(
-            <div></div>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                        <li><Link to = '/thetalks'>The Talks</Link></li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path = '/thetalks' component={TheTalks}/>
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 
