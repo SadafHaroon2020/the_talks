9f9f183300d4be094c173b667d450d8f9db29c99
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-24 22:56:39 -0500

    making components

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component.React{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+}
+export default NavigationBar
